ae3ff3f8f0a359ace753560d2ef6a261e19aa0be
Jack Humbert <jack.humb@gmail.com>
2021-11-15 00:39:12 -0500

    add rough buffer export & (really rough) import to text conversion

--- WolvenKit.Common/Conversion/RedFileDto.cs
+++ WolvenKit.Common/Conversion/RedFileDto.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using WolvenKit.Common.Model;
 using WolvenKit.Common.Tools;
 using WolvenKit.RED4.CR2W;
+using WolvenKit.Common.Oodle;
+using Splat;
 
 namespace WolvenKit.Common.Conversion
 {
@@ -12,7 +15,7 @@ namespace WolvenKit.Common.Conversion
 
         public Dictionary<int,RedExportDto> Chunks { get; set; } = new();
 
-        public List<RedBufferDto> Buffers { get; set; } = new();
+        public Dictionary<int, RedExportDto> Buffers { get; set; } = new();
 
         public bool ShouldSerializeBuffers() => Buffers is { Count: > 0 };
 
@@ -24,19 +27,45 @@ namespace WolvenKit.Common.Conversion
         public RedFileDto(IWolvenkitFile cr2w)
         {
             Chunks = cr2w.Chunks.ToDictionary(_ => _.ChunkIndex, _ => new RedExportDto(_));
-            Buffers = cr2w.Buffers.Select(_ => new RedBufferDto(_)).ToList();
+            foreach (var buffer in cr2w.Buffers)
+            {
+                var data = ((CR2WBufferWrapper)buffer).GetData();
+
+                using var uncompressedMS = new MemoryStream();
+                using (var compressedMs = new MemoryStream(data))
+                {
+                    OodleHelper.DecompressAndCopySegment(compressedMs, uncompressedMS, buffer.DiskSize, buffer.MemSize);
+                }
+
+                // try reading as normal cr2w file
+                var _parser = Locator.Current.GetService<Red4ParserService>();
+                var cr2wbuffer = _parser.TryReadCr2WFile(uncompressedMS);
+                if (cr2wbuffer != null)
+                {
+                    foreach (var _ in cr2wbuffer.Chunks)
+                    {
+                        Buffers.Add(_.ChunkIndex, new RedExportDto(_));
+                    }
+                }
+                // try reading as compiled package
+                else
+                {
+                    uncompressedMS.Seek(0, SeekOrigin.Begin);
+                    var compiledPackage = _parser.TryReadCompiledPackage(uncompressedMS);
+                    if (compiledPackage != null)
+                    {
+                        foreach (var _ in compiledPackage.Chunks)
+                        {
+                            Buffers.Add(_.ChunkIndex, new RedExportDto(_));
+                        }
+                    }
+                }
+            }
         }
 
         public CR2WFile ToW2rc()
         {
-            var cr2w = new CR2WFile
-            {
-                Buffers = Buffers
-                    .OrderBy(_ => _.Index)
-                    .Select(_ => _.ToRedBuffer())
-                    .ToList()
-            };
-
+            var cr2w = new CR2WFile();
             // chunks
             // order so that parent chunks get created first
             var groupedChunks = Chunks.GroupBy(_ => _.Value.ParentIndex);
@@ -45,6 +74,18 @@ namespace WolvenKit.Common.Conversion
                 foreach (var (chunkIndex, chunk) in groupedChunk.OrderBy(_ => _.Key))
                 {
                     chunk.CreateChunkInFile(cr2w, chunkIndex);
+                }
+            }
+            var last_key = cr2w.Chunks.Count();
+            // buffers
+            // might be naughty. maybe it works?
+            // we might need remove the compiledData field in the previous set of chunks
+            var groupedBuffers = Buffers.GroupBy(_ => _.Value.ParentIndex);
+            foreach (IGrouping<int, KeyValuePair<int, RedExportDto>> groupedBuffer in groupedBuffers)
+            {
+                foreach (var (bufferIndex, buffer) in groupedBuffer.OrderBy(_ => _.Key))
+                {
+                    buffer.CreateChunkInFile(cr2w, bufferIndex + last_key);
                 }
             }
 
